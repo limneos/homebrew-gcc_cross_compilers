5029a762a02ed0599cac64e12c23b6b44e670448
Elias Limneos <limneoselias@gmail.com>
2023-07-04 16:08:34 +0300

    Update i386-elf-gdb.rb

--- i386-elf-gdb.rb
+++ i386-elf-gdb.rb
@@ -9,10 +9,10 @@ class I386ElfGdb < Formula
   depends_on 'i386-elf-gcc'
 
   def install
-    ENV['CC'] = '/usr/local/opt/gcc/bin/gcc-7'
-    ENV['CXX'] = '/usr/local/opt/gcc/bin/g++-7'
-    ENV['CPP'] = '/usr/local/opt/gcc/bin/g++-7'
-    ENV['LD'] = '/usr/local/opt/gcc/bin/gcc-7'
+    ENV['CC'] = '/usr/local/bin/gcc-7'
+    ENV['CXX'] = '/usr/local/bin/g++-7'
+    ENV['CPP'] = '/usr/local/bin/g++-7'
+    ENV['LD'] = '/usr/local/bin/gcc-7'
 
     mkdir 'build' do
       system '../configure', '--target=i386-elf', "--prefix=#{prefix}", "--disable-werror"
