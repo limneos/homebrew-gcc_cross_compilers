27d679914966550c8532429344ced4576c343de6
Elias Limneos <limneoselias@gmail.com>
2023-07-04 16:05:17 +0300

    Update arm-elf-gdb.rb

--- arm-elf-gdb.rb
+++ arm-elf-gdb.rb
@@ -9,10 +9,10 @@ class ArmElfGdb < Formula
   depends_on 'arm-elf-gcc'
 
   def install
-    ENV['CC'] = '/usr/loca/bin/gcc-7'
-    ENV['CXX'] = '/usr/loca/bin/g++-7'
-    ENV['CPP'] = '/usr/loca/bin/cpp-7'
-    ENV['LD'] = '/usr/loca/bin/gcc-7'
+    ENV['CC'] = '/usr/local/bin/gcc-7'
+    ENV['CXX'] = '/usr/local/bin/g++-7'
+    ENV['CPP'] = '/usr/local/bin/cpp-7'
+    ENV['LD'] = '/usr/local/bin/gcc-7'
 
     mkdir 'build' do
       system '../configure', '--target=arm-elf-eabi', "--prefix=#{prefix}" ,'--disable-werror'
